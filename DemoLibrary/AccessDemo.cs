dbc6c1bfee3fcab1b83ee06fd9e1abd948a8f3ed
Octavio Arruda <octavio@maxiprod.com.br>
2020-12-26 19:40:55 -0300

    :tada: first commit

---
+++ DemoLibrary/AccessDemo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class AccessDemo
+    {
+        private void PrivateDemo()
+        {
+            
+        }
+
+        private protected void PrivateProtectedDemo()
+        {
+
+        }
+
+        protected void ProtectedDemo()
+        {
+
+        }
+
+        protected internal void ProtectedInternalDemo()
+        {
+
+        }
+
+        internal void InternalDemo()
+        {
+
+        }
+
+        public void PublicDemo()
+        {
+
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+# C# Access Modifiers
+
+## A motivação deste repositório é aprender sobre os `Access Modifiers` de C# e de outras linguagens, por consequência.
+
+### A palavra-chave `private`:
+- O código só estará acessível dentro da mesma classe
